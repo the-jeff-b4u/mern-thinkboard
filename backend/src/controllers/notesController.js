41300a1cd83ee90bc96a641e20e7b03edfff7920
the-jeff-b4u <jeffrickaldho@gmail.com>
2025-09-05 02:52:27 +0530

    connected to db

--- backend/src/controllers/notesController.js
+++ backend/src/controllers/notesController.js
@@ -1,13 +1,35 @@
-export function getAllNotes(req,res){
-    res.status(200).send("you got 50 notes");
+import Note from "../models/Notes.js"
+
+export async function getAllNotes(req,res){
+    try{
+        const notes = await Note.find()
+        res.status(200).json(notes)
+    } catch(error){
+        console.error("Error in getAllNotes controller", error);
+        res.status(500).json({message: "Internal server Error"});
+    }
 }
 
-export function createNote(req,res){
-    res.status(201).json({message : "post created successfully!"});
+export async function createNote(req,res){
+    try {
+        const {title,content} = req.body
+        const newNote = new Note({title, content})
+        await newNote.save()
+        res.status(201).json({message:"Note created succesfully"})
+    } catch (error){
+        console.error("Error in createNotes controller", error);
+        res.status(500).json({message: "Internal server Error"});
+    }
 }
 
-export function updateNote(req,res){
-    res.status(200).json({message : "post updated successfully!"})
+export async function updateNote(req,res){
+    try{
+        const {title,content} = req.body
+        await Note.findByIdAndUpdate(req.params.id,{title,content})
+        res.status(200).json({message: "Note updated successfully"})
+    } catch( error ){
+
+    }
 }
 
 export function deleteNote(req,res){
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,22 @@
 import express from "express"
 import notesRoutes from "./routes/notesRoutes.js";
+import {connectDB} from "./config/db.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+
 const app = express()
+const PORT = process.env.PORT || 5001
+
+connectDB();
+
+//middleware
+app.use(express.json());
 
 app.use("/api/notes", notesRoutes)
 
-app.listen(5001, () => {
-    console.log("Server started on PORT: 5001");
+app.listen(PORT, () => {
+    console.log("Server started on PORT:",PORT);
 });
 
+//mongodb+srv://jeffdhorick_db_user:GHJnpYbPhaJaB74L@cluster0.c1bhpo5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
