42a852692dae92f65c6bde80fc0fa7a2a037d388
the-jeff-b4u <jeffrickaldho@gmail.com>
2025-09-05 02:53:25 +0530

    connected to db

--- backend/src/controllers/notesController.js
+++ backend/src/controllers/notesController.js
@@ -25,10 +25,16 @@ export async function createNote(req,res){
 export async function updateNote(req,res){
     try{
         const {title,content} = req.body
-        await Note.findByIdAndUpdate(req.params.id,{title,content})
+        const updatedNote = await Note.findByIdAndUpdate(req.params.id,{title,content},
+            {
+                new: true,
+            }
+        )
+        if (!updateNote) return res.status(404).json({message:"Note not found"})
         res.status(200).json({message: "Note updated successfully"})
     } catch( error ){
-
+        console.error("Error in updateNotes controller", error);
+        res.status(500).json({message: "Internal server Error"});
     }
 }
 
