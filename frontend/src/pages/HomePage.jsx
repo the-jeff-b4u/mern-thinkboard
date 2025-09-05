07a04692ae0ef3310824f1f1487753b46aa4816a
the-jeff-b4u <jeffrickaldho@gmail.com>
2025-09-06 01:20:32 +0530

    frontend started

---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Navbar from '../components/NavBar';
+const HomePage = () => {
+  return (
+    <div className='min-h-screen'>
+      <Navbar />
+    </div>
+  )
+}
+
+export default HomePage
