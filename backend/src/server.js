125dba716f8957358c8b8e5f70f4fe01379f43be
the-jeff-b4u <jeffrickaldho@gmail.com>
2025-09-07 02:52:46 +0530

    Homepage

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,11 @@
 import express from "express"
+import cors from "cors";
+import dotenv from "dotenv";
+
 import notesRoutes from "./routes/notesRoutes.js";
 import {connectDB} from "./config/db.js";
-import dotenv from "dotenv";
 import rateLimiter from "./middleware/rateLimiter.js";
+
 
 dotenv.config();
 
@@ -10,8 +13,13 @@ const app = express()
 const PORT = process.env.PORT || 5001
 
 //middleware
+app.use(cors({
+    origin: "http://localhost:5173",
+})
+);
 app.use(express.json());
 app.use(rateLimiter);
+
 
 // app.use((req,res,next) => {
 //     console.log(`Req method is ${req.method} & req url is ${req.url}`);
