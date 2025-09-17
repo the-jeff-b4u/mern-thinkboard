da3d12789c3a21ec9bf9f863cfb781def920434e
the-jeff-b4u <jeffrickaldho@gmail.com>
2025-09-17 17:51:37 +0530

    final

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express"
 import cors from "cors";
 import dotenv from "dotenv";
+import path from "path";
 
 import notesRoutes from "./routes/notesRoutes.js";
 import {connectDB} from "./config/db.js";
@@ -11,12 +12,15 @@ dotenv.config();
 
 const app = express()
 const PORT = process.env.PORT || 5001
+const __dirname = path.resolve()
 
 //middleware
-app.use(cors({
-    origin: "http://localhost:5173",
-})
-);
+if (process.env.NODE_ENV !== "production"){
+    app.use(cors({
+        origin: "http://localhost:5173",
+    })
+    );
+}
 app.use(express.json());
 app.use(rateLimiter);
 
@@ -26,7 +30,15 @@ app.use(rateLimiter);
 //     next();
 // });
 
-app.use("/api/notes", notesRoutes)
+app.use("/api/notes", notesRoutes);
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")))
+
+    app.get("*",(req,res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    });
+}
 
 connectDB().then(() => {
     app.listen(PORT, () => {
